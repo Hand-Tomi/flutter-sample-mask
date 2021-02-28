8c8a0fa728a0709e6937fbcf5c75b02ec86d5092
Tomi <seaud5@gmail.com>
2021-03-01 01:42:57 +0900

    로딩 처리와 View 나누기

--- lib/viewmodel/store_model.dart
+++ lib/viewmodel/store_model.dart
@@ -3,6 +3,7 @@ import 'package:flutter_mask/model/store.dart';
 import 'package:flutter_mask/repository/store_repository.dart';
 
 class StoreModel with ChangeNotifier {
+  var isLoading = false;
   List<Store> stores = [];
   final _storeRepository = StoreRepository();
 
@@ -11,7 +12,11 @@ class StoreModel with ChangeNotifier {
   }
 
   Future fetch() async {
+    isLoading = true;
+    notifyListeners();
+
     stores = await _storeRepository.fetch();
+    isLoading = false;
     notifyListeners();
   }
 }
