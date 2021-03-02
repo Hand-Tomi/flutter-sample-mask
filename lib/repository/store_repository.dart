d5e023435c6ee7bf89ec847e756a94036b018b91
Tomi <seaud5@gmail.com>
2021-03-03 00:11:12 +0900

    현재 위치 기반으로 검색

--- lib/repository/store_repository.dart
+++ lib/repository/store_repository.dart
@@ -3,10 +3,10 @@ import 'package:flutter_mask/model/store.dart';
 import 'package:http/http.dart' as http;
 
 class StoreRepository {
-  Future<List<Store>> fetch() async {
+  Future<List<Store>> fetch(double lat, double lng) async {
     final stores = List<Store>();
     var url =
-        "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.266750168982206&lng=126.99996208282643&m=5000";
+        "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=$lat&lng=$lng&m=5000";
     var response = await http.get(url);
 
     final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
