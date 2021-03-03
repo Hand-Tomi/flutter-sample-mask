a9003ae9d4dadf2a27c37488b2af91ce18777fdb
Tomi <seaud5@gmail.com>
2021-03-04 00:06:12 +0900

    인터넷 연결이 안 되어 있을 때의 처리

--- lib/repository/store_repository.dart
+++ lib/repository/store_repository.dart
@@ -10,24 +10,33 @@ class StoreRepository {
     final stores = List<Store>();
     var url =
         "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=$lat&lng=$lng&m=5000";
-    var response = await http.get(url);
 
-    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
-    final jsonStores = jsonResult['stores'];
+    try {
+      var response = await http.get(url);
 
-    jsonStores.forEach((e) {
-      final store = Store.fromJson(e);
-      final km = _distance.as(LengthUnit.Kilometer,
-          new LatLng(store.lat, store.lng), new LatLng(lat, lng));
-      store.km = km;
-      stores.add(store);
-    });
-    print('fetch완료');
-    return stores.where((e) {
-      return e.remainStat == 'plenty' ||
-          e.remainStat == 'some' ||
-          e.remainStat == 'few';
-    }).toList()
-      ..sort((a, b) => a.km.compareTo(b.km));
+      if (response.statusCode == 200) {
+        final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+        final jsonStores = jsonResult['stores'];
+
+        jsonStores.forEach((e) {
+          final store = Store.fromJson(e);
+          final km = _distance.as(LengthUnit.Kilometer,
+              new LatLng(store.lat, store.lng), new LatLng(lat, lng));
+          store.km = km;
+          stores.add(store);
+        });
+        print('fetch완료');
+        return stores.where((e) {
+          return e.remainStat == 'plenty' ||
+              e.remainStat == 'some' ||
+              e.remainStat == 'few';
+        }).toList()
+          ..sort((a, b) => a.km.compareTo(b.km));
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
   }
 }
